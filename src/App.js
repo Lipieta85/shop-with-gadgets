eb6962c2be9dfd6d3dcddbb80b70577a2a53ea00
unknown <rafal.lipieta1@wp.pl>
2019-10-08 08:22:08 +0200

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import NavMenu from './components/NavMenu';
+import Slider from './components/Slider';
+import LoginForm from './components/LoginForm';
+import Rules from './components/Rules';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavMenu />
+      <LoginForm />
+      <Rules />
+      <Slider />
     </div>
   );
 }
---
+++ src/components/Rules.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Rules = () => {
+    return (
+        <div>
+            <h1>
+                CZYTELNE ZASADY!
+            </h1>
+            <div className="col-lg-12">
+                <div className="rules">
+                    <div className="rule"></div>
+                    <div className="rule"></div>
+                    <div className="rule"></div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Rules;
