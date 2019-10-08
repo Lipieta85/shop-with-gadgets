9c931d60860cf7d8d0064e3e9b963b94501593b9
unknown <rafal.lipieta1@wp.pl>
2019-10-08 22:59:53 +0200

    Add Carousel and change project width

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import NavMenu from './components/NavMenu';
 import Slider from './components/Slider';
 import LoginForm from './components/LoginForm';
 import Rules from './components/Rules';
+import Carousel from './components/Carousel';
 
 function App() {
   return (
