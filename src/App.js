684949a0ffd095c4b2b29d7970df9fa215935a05
r.lipieta <rafal.lipieta1@wp.pl>
2020-02-28 15:23:17 +0100

    Small change in App.js

--- src/App.js
+++ src/App.js
@@ -56,7 +56,7 @@ function initializeReactGA() {
 }
 
 export default withRouter(function App({ location }, props) {
-    //const company = useSelector(state => state.clientDataReducer.companyId);
+    const company = useSelector(state => state.clientDataReducer.companyId);
     const lang = useSelector(state => state.clientDataReducer.language);
     const parsed = queryString.parse(location.search);
 
@@ -74,12 +74,12 @@ export default withRouter(function App({ location }, props) {
 
     useEffect(() => {
         document.body.classList.remove("theme-dark", "theme-light");
-        if (parsed.brand === "wix") {
+        if (company === "wix") {
             document.body.classList.add("theme-dark");
         } else {
             document.body.classList.add("theme-light");
         }
-    }, [parsed.brand]);
+    }, [company]);
 
     useEffect(() => {
         i18n.changeLanguage(lang);
