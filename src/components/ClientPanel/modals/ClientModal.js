788f6973103c024fdcda6b5732a3363f7f653b17
r.lipieta <rafal.lipieta1@wp.pl>
2020-03-24 09:41:16 +0100

    Fix bug in Order List

--- src/components/ClientPanel/modals/ClientModal.js
+++ src/components/ClientPanel/modals/ClientModal.js
@@ -6,7 +6,9 @@ import { useTranslation } from "react-i18next";
 import "../../../assets/styles/order-choose-modal.scss";
 const ClientModal = props => {
     const clientEmail = useSelector(
-        state => state.clientDataReducer.clientData.getWixClientData.userEmail,
+        state =>
+            state.clientDataReducer.clientData.getWixClientData &&
+            state.clientDataReducer.clientData.getWixClientData.userEmail,
     );
     const lang = useSelector(state => state.clientDataReducer.language);
     const [productId, setProductId] = useState();
@@ -35,8 +37,7 @@ const ClientModal = props => {
         >
             <Modal.Header closeButton>
                 <Modal.Title id="contained-modal-title-vcenter">
-                    {t("ClientModal.NotifyWhenProductAvailable")}:{" "}
-                    {props.name}
+                    {t("ClientModal.NotifyWhenProductAvailable")}: {props.name}
                 </Modal.Title>
             </Modal.Header>
 
