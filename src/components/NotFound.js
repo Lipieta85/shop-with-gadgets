8d309cae65fce03bc6dea881d76c87003f8c6064
m.wilczewski4 <m.wilczewski4@o2.pl>
2020-03-30 09:52:06 +0200

    translations

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -1,5 +1,6 @@
 import React, { useEffect } from "react";
 import { useDispatch } from "react-redux";
+import { useTranslation } from "react-i18next";
 import "../assets/styles/order-end.scss";
 import { signOut } from "../actions/authorization";
 import host from "../api/host";
@@ -7,6 +8,7 @@ import host from "../api/host";
 export default function NotFound() {
     const dispatch = useDispatch();
     const link = `${host}/site/desktop`;
+    const { t } = useTranslation();
 
     useEffect(() => {
         dispatch(signOut());
@@ -15,9 +17,9 @@ export default function NotFound() {
     return (
         <div className="order-end text-center">
             <div className="order-end-box">
-                <h3>Twoje dane autoryzacyjne wygasły, zaloguj sie ponownie</h3>
+                <h3>{t("NotFound.AuthDataExpired")}</h3>
                 <a className="btn btn-outline-primary mt-4" href={link}>
-                    Wróć do strony logowania
+                    {t("NotFound.GoToLog")}
                 </a>
             </div>
         </div>
